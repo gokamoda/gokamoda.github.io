961c8bd8c6f53f4383332ba97dac392e24b130b5
gokamoda <gokamoda@users.noreply.github.com>
2026-06-09 05:25:23 +0000

    deploy: 14b3dd2ff768cbef23f93f399ed89b43e9085486

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -204,7 +204,7 @@ var store = [{
         "teaser": null
       },{
         "title": "✅ Paper accepted to the ICML 2026",
-        "excerpt":"We are pleased to announce that our paper has been accepted to ICML 2026.                                Masataka Yoneda,           Yusuke Matsushita,                Go Kamoda,           Kohei Suenaga,           Takuya Akiba,           Masaki Waga,      &amp;           Sho Yokoi  (2026).  SoftMatcha 2: A Fast and Soft Pattern Matcher for Trillion-Scale Corpora.  In arXiv [cs.CL].                              Project Page     &nbsp;                                    arXiv                 -&gt; Publications  ","categories": ["News"],
+        "excerpt":"We are pleased to announce that our paper has been accepted to ICML 2026.                                Masataka Yoneda,           Yusuke Matsushita,                Go Kamoda,           Kohei Suenaga,           Takuya Akiba,           Masaki Waga,      &amp;           Sho Yokoi  (2026).  SoftMatcha 2: A Fast and Soft Pattern Matcher for Trillion-Scale Corpora.  In Forty-third International Conference on Machine Learning.                              Project Page     &nbsp;                                    arXiv                 -&gt; Publications  ","categories": ["News"],
         "tags": [],
         "url": "/news/icml2026-accept/",
         "teaser": null
@@ -356,7 +356,7 @@ var store = [{
         "title": "SoftMatcha 2: A Fast and Soft Pattern Matcher for Trillion-Scale Corpora",
         "excerpt":" ","categories": [],
         "tags": [],
-        "url": "/202602-arxiv-yoneda",
+        "url": "/202607-icml-yoneda",
         "teaser": null
       },{
         "title": "Language Models Compare Quantities Using Number-specific and Unit-specific Heuristics",
